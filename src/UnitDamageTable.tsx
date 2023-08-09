7b5e5de2b134e39cd46037539ff8dbc6e779f70b
Björn Eriksson <bjorn.eriksson@regionvasterbotten.se>
2023-08-09 15:37:11 +0200

    Fixed unit damage table

--- src/UnitDamageTable.tsx
+++ src/UnitDamageTable.tsx
@@ -1,46 +1,27 @@
 import React from 'react';
-import { DetailsList, DetailsListLayoutMode, IColumn, SelectionMode } from '@fluentui/react';
 
 interface UnitDamageTableProps {
     unit: Unit;
 }
 
 const UnitDamageTable: React.FC<UnitDamageTableProps> = ({ unit }) => {
-    const columns: IColumn[] = [
-        { key: 'S', name: 'S (+0)', fieldName: 'S', minWidth: 50, maxWidth: 100 },
-        { key: 'M', name: 'M (+2)', fieldName: 'M', minWidth: 50, maxWidth: 100 },
-        { key: 'L', name: 'L (+4)', fieldName: 'L', minWidth: 50, maxWidth: 100 },
-    ];
-
-    const data = [
-        {
-            key: '1',
-            S: unit.BFDamageShort.toString(),
-            M: unit.BFDamageMedium.toString(),
-            L: unit.BFDamageLong.toString(),
-        },
-    ];
-
     return (
-        <DetailsList
-            items={data}
-            columns={columns}
-            selectionMode={SelectionMode.none}
-            onRenderDetailsHeader={props => (
-                <div
-                    style={{
-                        backgroundColor: 'black',
-                        color: 'white',
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                    }}
-                >
-                    <div style={{ width: props!.columns[0].calculatedWidth! }}>S (+0)</div>
-                    <div style={{ width: props!.columns[1].calculatedWidth! }}>M (+2)</div>
-                    <div style={{ width: props!.columns[2].calculatedWidth! }}>L (+4)</div>
-                </div>
-            )}
-        />
+        <table style={{ width: '100%', borderCollapse: 'collapse', textAlign: 'center' }}>
+            <thead style={{ backgroundColor: 'black', color: 'white' }}>
+                <tr>
+                    <th style={{ minWidth: 50, maxWidth: 100 }}>S (+0)</th>
+                    <th style={{ minWidth: 50, maxWidth: 100 }}>M (+2)</th>
+                    <th style={{ minWidth: 50, maxWidth: 100 }}>L (+4)</th>
+                </tr>
+            </thead>
+            <tbody style={{ backgroundColor: 'lightgray' }}>
+                <tr>
+                    <td style={{ minWidth: 50, maxWidth: 100, color: 'red', fontWeight: 'bold' }}>{unit.BFDamageShort}</td>
+                    <td style={{ minWidth: 50, maxWidth: 100, color: 'red', fontWeight: 'bold' }}>{unit.BFDamageMedium}</td>
+                    <td style={{ minWidth: 50, maxWidth: 100, color: 'red', fontWeight: 'bold' }}>{unit.BFDamageLong}</td>
+                </tr>
+            </tbody>
+        </table>
     );
 };
 
