1da92ef8a622502e2bcfb7ea2ab707a3ce6ac3bc
Björn Eriksson <bjorneriksson@gmail.com>
2023-08-10 21:54:32 +0200

    Weapon criticals now reduce damage

--- src/AttackDamageTable.tsx
+++ src/AttackDamageTable.tsx
@@ -5,7 +5,17 @@ interface AttackDamageTableProps {
     unit: Unit;
 }
 
+const calculateAdjustedDamage = (baseDamage: number, weaponHits: number) => {
+    return Math.max(0, baseDamage - weaponHits);
+};
+
 const AttackDamageTable: React.FC<AttackDamageTableProps> = ({ unit }) => {
+    const weaponHits = unit.MyWeaponHits ?? 0;
+
+    const adjustedShort = calculateAdjustedDamage(unit.BFDamageShort, weaponHits);
+    const adjustedMedium = calculateAdjustedDamage(unit.BFDamageMedium, weaponHits);
+    const adjustedLong = calculateAdjustedDamage(unit.BFDamageLong, weaponHits);
+
     return (
         <table style={{ width: '100%', borderCollapse: 'collapse', textAlign: 'center', border: 'solid black', marginBottom: 5 }}>
             <thead style={{ backgroundColor: 'black', color: 'white', borderRadius: 10 }}>
@@ -17,9 +27,9 @@ const AttackDamageTable: React.FC<AttackDamageTableProps> = ({ unit }) => {
             </thead>
             <tbody style={{ backgroundColor: 'lightgray' }}>
                 <tr>
-                    <td style={{ minWidth: 50, maxWidth: 100, color: 'darkred', fontWeight: 'bold', fontSize: 20 }}>{unit.BFDamageShort}</td>
-                    <td style={{ minWidth: 50, maxWidth: 100, color: 'darkred', fontWeight: 'bold', fontSize: 20 }}>{unit.BFDamageMedium}</td>
-                    <td style={{ minWidth: 50, maxWidth: 100, color: 'darkred', fontWeight: 'bold', fontSize: 20 }}>{unit.BFDamageLong}</td>
+                    <td style={{ minWidth: 50, maxWidth: 100, color: 'darkred', fontWeight: 'bold', fontSize: 20 }}>{adjustedShort}</td>
+                    <td style={{ minWidth: 50, maxWidth: 100, color: 'darkred', fontWeight: 'bold', fontSize: 20 }}>{adjustedMedium}</td>
+                    <td style={{ minWidth: 50, maxWidth: 100, color: 'darkred', fontWeight: 'bold', fontSize: 20 }}>{adjustedLong}</td>
                 </tr>
             </tbody>
         </table>
